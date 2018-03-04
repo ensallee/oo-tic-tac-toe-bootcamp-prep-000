60d7718722469524fa27fb36926483191c425548
Betsy Sallee <ensallee@gmail.com>
2018-03-04 21:37:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,4 +69,39 @@ def current_player
   turn_count % 2 == 0 ? "X" : "O"
 end
 
+def won?
+    WIN_COMBINATIONS.each do |combination|
+    win_index_1=combination[0]
+    win_index_2=combination[1]
+    win_index_3=combination[2]
+
+    position_1=@board[win_index_1]
+    position_2=@board[win_index_2]
+    position_3=@board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return combination
+    else
+      false
+    end
+  end
+  return false
+end
+
+def full?
+  @board.all? do |index|
+    index == "X" || index == "O"
+  end
+end
+
+def draw?
+  if !won? && full?
+    true
+  else
+    false
+  end
+end
+
 end
